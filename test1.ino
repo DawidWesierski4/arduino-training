91aaebe66a581935f0f92d196ced30f0154ae759
dawidW4 <dawidwesierski4@gmail.com>
2024-11-02 14:12:11 +0100

    led change both ways

--- test1.ino
+++ test1.ino
@@ -1,23 +1,23 @@
 #define PORT_PWM_DIOD_RED 3
-int fill_factor, change;
+int change;
+byte fill_factor;
 
 void setup() {
   Serial.begin(9600);
   pinMode(PORT_PWM_DIOD_RED, OUTPUT);
   fill_factor = 0;
-  change = 2;
+  change = 4;
   Serial.println("[INFO] Starting the PWM Arduino program");
 }
 
 void loop() {
   analogWrite(PORT_PWM_DIOD_RED, fill_factor);
 
-  if(fill_factor < 255) {
-    fill_factor += change;
-  } else {
-    fill_factor = 0;
+  fill_factor += change;
+  if (fill_factor > (255 - abs(change)) || fill_factor < abs(change)) {
+    change *= -1;
+    Serial.println("[INFO] change switch polarization change = " + String(change));
   }
-
-  delay(50);
+  delay(40);
 
 }
